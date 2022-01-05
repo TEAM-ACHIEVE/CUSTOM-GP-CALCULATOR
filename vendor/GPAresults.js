3a3fa254349e6d7d5c2f1d6ee4093ffc70111592
Mmerichukwu Anosike <67147759+Roarnotes@users.noreply.github.com>
2022-01-05 06:45:18 +0100

    Instruction boxs display set for cgpa

--- vendor/GPAresults.js
+++ vendor/GPAresults.js
@@ -20,9 +20,13 @@ function printPage() {
 //function that help to remove the instruction bar and page submit buttons  add the name of the student
 function instructionBOXremover() {
   document.getElementById("insturctionbox1").style.display = "none";
+  document.getElementById("insturctionbox2").style.display = "none";
+  document.getElementById("insturctionbox3").style.display = "none";
+
 
   // removing the button
   document.getElementById("finalbuttonREMOVER1").style.display = "none";
+  document.getElementById("finalbuttonREMOVER2").style.display = "none";
 }
 
 function theNamerchanger(){
